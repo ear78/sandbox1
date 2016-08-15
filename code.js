e81e660d4ae388426caa7fa39961ec0757e02cb3
Elliot Richardson <elliotrichardson@Elliots-MacBook-Pro.local>
2016-08-15 14:46:12 -0600

    this a new file and change for code

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 var journal = ["dm13 rocks!!", "this is another"];
+
+/*
+cd foldername
+cd ..
+cd ~
+ls
+mkdir newfoldername
+touch newfileName.txt
+
+git clone
+git status
+git add filename.js
+git commit -m "message"
+git push
+
+*/
