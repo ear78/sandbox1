7b695ad0c4c66c6181bdf651519080dbec15f888
Elliot Richardson <elliotrichardson@Elliots-MacBook-Pro.local>
2016-08-15 15:11:03 -0600

    This is for branch2

--- code.js
+++ code.js
@@ -8,7 +8,11 @@ var something = "This is another edit";
 
 var string = "Another string";
 
-/*
+var branch2 = "this is for branch2";
+
+
+/* This is for using git and going in and out of directories
+
 cd foldername
 cd ..
 cd ~
@@ -16,10 +20,17 @@ ls
 mkdir newfoldername
 touch newfileName.txt
 
+////////////////////////////////////////////
 git clone
 git status
 git add filename.js
 git commit -m "message"
 git push
+git add -A *adds all files that need to be added
+
+/////////////////////////////////////////////
+git checkout -b branchName
+git checkout branchName
+
 
 */
